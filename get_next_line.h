4c662b012c40c7d41e1d01e69bae66ef23b4361b
Yasmin <yasmin27101991@gmail.com>
2024-11-04 19:18:32 +0100

    functions aditionals

--- get_next_line.h
+++ get_next_line.h
@@ -3,21 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yasjimen <yasjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:52:54 by yasjimen          #+#    #+#             */
-/*   Updated: 2024/11/02 19:10:52 by yasjimen         ###   ########.fr       */
+/*   Updated: 2024/11/04 18:52:35 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 
-void	copy_content(char *dest, char *src, size_t *index);
-char	join_buffers(char *existing_buf, char *new_buf);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin_free(char *s1, char *s2);
+char	*ft_strdup(const char *s);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,41 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yasjimen <yasjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:52:43 by yasjimen          #+#    #+#             */
-/*   Updated: 2024/11/02 19:07:33 by yasjimen         ###   ########.fr       */
+/*   Updated: 2024/11/04 19:02:56 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
 
-void	copy_content(char *dest, char *src, size_t *index)
+size_t	ft_strlen(const char *s)
 {
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*result;
+	size_t	len1;
+	size_t	len2;
 	size_t	i;
 
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	result = (char *)malloc(len1 + len2 + 1);
+	if (!result)
+		return (NULL);
 	i = 0;
-	while(src[1])
+	while (i < len1)
 	{
-		dest[*index] = src[i];
-		(*index)++;
+		result[i] = s1[i];
 		i++;
 	}
-
-}
-
-char	join_buffers(char *existing_buf, char *new_buf)
-{
-	char	*combined_buf;
-	size_t	exist_idx;
-	size_t	new_idx;
-
-	if (!existing_buf)
+	while (i < len1 + len2)
 	{
-		existing_buf = (char *)malloc(1);
-		if (!existing_buf)
-			return (NULL);
-		existing_buf[0] = '\0';
+		result[i] = s2[i - len1];
+		i++;
 	}
+	result[len1 + len2] = '\0';
+	free(s1);
+	return (result);
 }
+
+
+char	*ft_strdup(const char *s)
+{
+	size_t	len;
+	char	*dup;
+	size_t	i;
+
+	len = ft_strlen(s);
+	dup = (char *)malloc(len + 1);
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[len] = '\0';
+	return (dup);
+}
+
+
